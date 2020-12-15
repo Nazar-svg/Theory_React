b4f7d63eca00f88607ed359768b17b7975665373
Nazar-svg <nazar.kolos90@gmail.com>
2020-12-15 17:08:45 +0200

    add total

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 
-
 export default class Counter extends Component {
-    state = {
-        counter: 0
-    }
-    onPlas = () => {
-      this.setState((prevState) => {
-          return {
-            counter : prevState.counter + 1
-          } 
-      })
-    }
-    
-    render() {
-        return (
-          <>
-          <h1>Counter {this.state.counter}</h1>
-          <button onClick={this.onPlas}>Plus</button>
-          <button onClick={() => this.setState({counter: this.state.counter - 1})}>Minus</button>
-          </>
-        )
-    }
+  state = {
+    counter: 0,
+    total: 10,
+  };
+  onPlas = () => {
+    this.setState((prevState) => {
+      return {
+        counter: prevState.counter + 1,
+      };
+    });
+  };
 
+  render() {
+    return (
+      <>
+        <h1>Counter {this.state.counter}</h1>
+        <button onClick={this.onPlas}>Plus</button>
+        <button
+          onClick={() => this.setState({ counter: this.state.counter - 1 })}
+        >
+          Minus
+        </button>
+      </>
+    );
+  }
 }
